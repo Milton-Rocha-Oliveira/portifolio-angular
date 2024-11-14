1a1bb5dbef3c593755b4bfe48b16adc0e05c43ae
milton rocha <milton.j.r.oliveira.1990@gmail.com>
2024-11-13 22:37:26 -0300

    quase pronto

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component , Input } from '@angular/core';
+import { ProjectComponent } from "./project/project.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
 
+  @Input()
+  imgProject:string = ""
+  @Input()
+  textProject:string = ""
+  @Input()
+  linkProject:string = ""
 }
