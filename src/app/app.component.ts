cc6e37307def978701f717fdb354f762918a71ca
milton rocha <milton.j.r.oliveira.1990@gmail.com>
2024-11-20 06:52:16 -0300

    reponsividade max-width:800px

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { NgOptimizedImage } from '@angular/common';
 import { CertificatesPageComponent } from "./pages/certificates-page/certificates-page.component";
 import { ProjectsPageComponent } from "./pages/projects-page/projects-page.component";
 import { ContactComponent } from "./components/contact/contact.component";
+import { CertificatesComponent } from "./components/certificates/certificates.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NgOptimizedImage, CertificatesPageComponent, ProjectsPageComponent, ContactComponent],
+  imports: [RouterOutlet, HomeComponent, NgOptimizedImage, CertificatesPageComponent, ProjectsPageComponent, ContactComponent, CertificatesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
